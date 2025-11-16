3e9411f44171ecaf10528f0cd04fa64084f64268
yhobany <68508255+yhobany@users.noreply.github.com>
2025-11-16 12:21:40 -0500

    🚀 Deploy on 2025-11-16T12:21:38.800901

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2728351165"
+    serviceWorkerVersion: "1609528801"
   }
 });
